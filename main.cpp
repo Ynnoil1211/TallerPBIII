6834f556165702915ef93c000cb26ee4777bf14e
Daniel Borre <danisecundarioxd@gmail.com>
2025-10-22 23:02:45 -0500

    Defini las estructuras

--- main.cpp
+++ main.cpp
@@ -1,5 +1,45 @@
 #include <iostream>
+#include <vector>
 using namespace std;
+
+struct Usuario {
+    int idUsuario;
+    string nombre, correoElectronico, contraseña, direccion, metodoDePago;
+};
+
+struct Producto {
+    int idProducto;
+    string nombre, descripcion;
+    double precio;
+    int stock;
+};
+
+struct CarritoDeCompras {
+    int idCarrito;
+    Usuario usuario;
+    vector<int> productos;
+    double subtotal, impuestos;
+};
+
+struct OrdenDeCompra {
+    int idOrden;
+    vector<Producto> productos;
+    double subtotal, impuestos, envio, total;
+};
+
+struct Categoria {
+    int idCategoria;
+    string nombre;
+};
+
+struct Comentarios {
+    int idComentarios;
+    Producto producto;
+    Usuario usuario;
+    string comentario;
+    string fecha;
+};
+
 int main(){
 
     return 0;
